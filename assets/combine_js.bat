67f2d7ddc44deecee8b461fd5197c0e310bde145
Paul Koppen <p.koppen@surrey.ac.uk>
2018-06-21 14:33:21 +0100

    Move data root declaration into html data element on the body tag.

--- assets/combine_js.bat
+++
@@ -1,7 +0,0 @@
-del all.js
-type OrbitControls.js >> all.js
-type ImageDataLoader.js >> all.js
-type MapLoader.js >> all.js
-type main.js >> all.js
-echo Go to: http://refresh-sf.com/ or https://yui.2clics.net/ and upload all.js.
-echo Download the compressed result as min.js.
--- assets/main.js
+++ assets/main.js
@@ -1,67 +1,64 @@
-var root = '//files.facer2vm.org/view/';
-root = 'data/';
+(function(){
+    
+    var camera;
+    var controls;
+    var renderer;
+    var scene;
 
-var camera;
-var controls;
-var renderer;
-var scene;
+    init();
+    animate();
 
-var clock = new THREE.Clock();
+    function init() {
+        var container = document.getElementById('container');
+        var dl_button = document.getElementById('dlbtn');
 
-init();
-animate();
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xbfd1e5);
 
-function init() {
-  var container = document.getElementById('container');
-  var dl_button = document.getElementById('dlbtn');
-  
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xbfd1e5);
-  
-  if (location.hash) {
-    var loader = new THREE.MapLoader();
-    var name = location.hash.substr(1).match(/[\w-]*/)[0];
-    var mapfile = name + '.map';
-    var zipfile = name + '.zip';
-    loader.setPath(root);
-    loader.load(mapfile, function (mesh) {scene.add(mesh);});
-    dl_button.href = root + zipfile;
-  }
-  
-  camera = new THREE.PerspectiveCamera(50, 4/3, 1, 20000);
-  camera.position.z = 400;
-  camera.up = new THREE.Vector3(0, 1, 0);
-  camera.lookAt(scene.position);
-  
-  renderer = new THREE.WebGLRenderer({antialias:true});
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(400, 300);
-  
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.addEventListener('change', render);
-  
-  container.innerHTML = '';
-  container.appendChild(renderer.domElement);
-  
-  window.addEventListener('resize', onWindowResize, false);
-  onWindowResize();
-}
+        if (location.hash) {
+            var root = document.body.dataset.root;
+            var loader = new THREE.MapLoader();
+            var name = location.hash.substr(1).match(/[\w-]*/)[0];
+            var mapfile = name + '.map';
+            var zipfile = name + '.zip';
+            loader.setPath(root);
+            loader.load(mapfile, function (mesh) {scene.add(mesh);});
+            dl_button.href = root + zipfile;
+        }
 
-function onWindowResize() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  camera.aspect = w / h;
-  camera.updateProjectionMatrix();
-  renderer.setSize(w, h);
-  //controls.handleResize();
-}
+        camera = new THREE.PerspectiveCamera(50, 4/3, 1, 20000);
+        camera.position.z = 400;
+        camera.up = new THREE.Vector3(0, 1, 0);
+        camera.lookAt(scene.position);
 
-function animate() {
-  requestAnimationFrame(animate);
-  render();
-}
+        renderer = new THREE.WebGLRenderer({antialias:true});
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(400, 300);
 
-function render() {
-  //controls.update(clock.getDelta());
-  renderer.render(scene, camera);
-}
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.addEventListener('change', render);
+
+        container.innerHTML = '';
+        container.appendChild(renderer.domElement);
+
+        window.addEventListener('resize', onWindowResize, false);
+        onWindowResize();
+    }
+
+    function onWindowResize() {
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        camera.aspect = w / h;
+        camera.updateProjectionMatrix();
+        renderer.setSize(w, h);
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+    }
+
+    function render() {
+        renderer.render(scene, camera);
+    }
+})();
