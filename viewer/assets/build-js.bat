6a794ca8bbe3102160b6e0d4e1fd9c1019ca13bc
Paul Koppen <p.koppen@surrey.ac.uk>
2018-07-05 23:23:29 +0100

    Restructure.

---
+++ viewer/assets/build-js.bat
@@ -0,0 +1,11 @@
+del all.js
+type OrbitControls.js >> all.js
+type ImageDataLoader.js >> all.js
+type MapLoader.js >> all.js
+type main.js >> all.js
+@echo
+@echo ---------------------------------------------------------------------------
+@echo Go to: http://refresh-sf.com/ or https://yui.2clics.net/ and upload all.js.
+@echo Copy the compressed result and paste in index.html.
+@echo ---------------------------------------------------------------------------
+@echo
